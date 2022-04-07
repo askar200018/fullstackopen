b303af3512ea85d6c38a74b588601a82c073dbcc
askar-dev <askar200018@mail.ru>
2022-04-07 17:36:11 +0600

    1.8: unicafe step3

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,17 +1,16 @@
 import { useState } from 'react';
 
-const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-
+const Statistics = ({ good, neutral, bad }) => {
   const sumOfAllFeedbacks = () => {
     return good + neutral + bad;
   };
 
   const getAverageFeedback = () => {
-    return (good * 1 + neutral * 0 + bad * -1) / sumOfAllFeedbacks();
+    const sumOfAll = sumOfAllFeedbacks();
+    if (sumOfAll === 0) {
+      return 0;
+    }
+    return (good * 1 + neutral * 0 + bad * -1) / sumOfAll;
   };
 
   const getPercentageOfPositiveFeedback = () => {
@@ -24,21 +23,31 @@ const App = () => {
 
   return (
     <div>
+      <h2>statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {sumOfAllFeedbacks()}</p>
+      <p>average {getAverageFeedback()}</p>
+      <p>positive {getPercentageOfPositiveFeedback()}%</p>
+    </div>
+  );
+};
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <div>
       <div>
         <h2>give feedback</h2>
         <button onClick={() => setGood(good + 1)}>good</button>
         <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
         <button onClick={() => setBad(bad + 1)}>bad</button>
       </div>
-      <div>
-        <h2>statistics</h2>
-        <p>good {good}</p>
-        <p>neutral {neutral}</p>
-        <p>bad {bad}</p>
-        <p>all {sumOfAllFeedbacks()}</p>
-        <p>average {getAverageFeedback()}</p>
-        <p>positive {getPercentageOfPositiveFeedback()}%</p>
-      </div>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
