eae63e3d118ad4cb4ba27d37c01b4998f0a97896
askar-dev <askar200018@mail.ru>
2022-04-11 14:29:28 +0600

    2.8: The Phonebook Step3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,15 +1,25 @@
 import { useState } from 'react';
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', phone: '040-1234567' },
+  ]);
   const [newName, setNewName] = useState('');
+  const [newPhone, setNewPhone] = useState('');
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
   };
 
+  const handlePhoneChange = (event) => {
+    setNewPhone(event.target.value);
+  };
+
   const addPerson = (event) => {
     event.preventDefault();
+    if (newName.trim() === '' || newPhone.trim() === '') {
+      return;
+    }
 
     const isPersonExist = persons.find((person) => person.name === newName);
 
@@ -20,10 +30,12 @@ const App = () => {
 
     const personObject = {
       name: newName,
+      phone: newPhone,
     };
 
     setPersons([...persons, personObject]);
     setNewName('');
+    setNewPhone('');
   };
 
   return (
@@ -34,13 +46,18 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          number: <input value={newPhone} onChange={handlePhoneChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
         {persons.map((person) => (
-          <li key={person.name}>{person.name}</li>
+          <li key={person.name}>
+            {person.name} {person.phone}
+          </li>
         ))}
       </ul>
     </div>
