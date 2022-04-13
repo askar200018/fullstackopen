aa261bac927ce9863a803aec1dd347396261cc55
askar200018 <imangali950@gmail.com>
2022-04-13 11:22:55 +0600

    2.11: The Phonebook Step6

--- part2/phonebook/src/index.js
+++ part2/phonebook/src/index.js
@@ -1,13 +1,11 @@
+import ReactDOM from 'react-dom';
 import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
 
 import App from './App';
 
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
-
-root.render(
+ReactDOM.render(
   <StrictMode>
     <App />
-  </StrictMode>
+  </StrictMode>,
+  document.getElementById('root')
 );
