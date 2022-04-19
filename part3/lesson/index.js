18422e66ab07db700eacda9a059730bd37cfb4da
askar200018 <imangali950@gmail.com>
2022-04-19 13:54:49 +0600

    a. -Receiving data

--- part3/lesson/index.js
+++ part3/lesson/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 
 let notes = [
   {
@@ -45,6 +46,33 @@ app.delete("/api/notes/:id", (request, response) => {
   notes = notes.filter((note) => note.id !== id);
 
   response.status(204).end();
+});
+
+const generateId = () => {
+  const maxId =
+    notes.length > 0 ? Math.max(...notes.map((note) => note.id)) : 0;
+  return maxId + 1;
+};
+
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const note = {
+    id: generateId(),
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+  };
+
+  notes = notes.concat(note);
+
+  response.json(note);
 });
 
 const PORT = 3001;
